4d86fb9aa75d3483405812b0361cd188979c0659
Josh Beard <josh@jlbeard.com>
2022-08-23 23:22:21 -0400

    Added debug logic to init

--- app.ts
+++ app.ts
@@ -5,9 +5,13 @@ console.log("Hello fushigi");
 const canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
 const context = canvas.getContext("2d") as CanvasRenderingContext2D;
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const isDebug = urlParams.get("debug");
+
 const game = new Game(
     canvas, 
     context,
-    true);
+    isDebug == "true" || isDebug == "True");
 
 game.run();
