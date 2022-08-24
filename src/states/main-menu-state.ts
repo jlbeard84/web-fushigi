8a47c7e5cb204cc70e6959456f79c0aca93c271d
Josh Beard <josh@jlbeard.com>
2022-08-23 23:06:45 -0400

    Added placeholder to main menu state

--- src/states/main-menu-state.ts
+++ src/states/main-menu-state.ts
@@ -1,15 +1,30 @@
 import { DrawManager, GameState, IntroState } from "../";
 
+const durationThreshold: number = 100;
+const counterThreshold: number = 254;
+
 class MainMenuState implements GameState {
 
     readonly name: string = "MainMenu";
 
-    update(duration: number): void {
+    private counter: number = 0;
+    private durationSum: number = 0;
 
+    update(duration: number): void {
+        this.durationSum += duration;
+
+        if (this.durationSum >= durationThreshold) {
+            this.durationSum = 0;
+            this.counter++;
+        }
+
+        if (this.counter > counterThreshold) {
+            this.counter = 0;
+        }
     }
 
     draw(drawManager: DrawManager) {
-
+        drawManager.setFullScreenColor(0, 0, this.counter);
     }
 
     finish(): GameState {
